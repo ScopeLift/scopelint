8e823f9c5eb1cbe5162565008828b08ac307777c
John Feras <jferas@ferasinfotech.com>
2022-11-08 15:19:10 -0500

    fix: don't check non-solidity files

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs, unreachable_pub, unused, rust_2021_compatibility)]
 #![warn(clippy::all, clippy::pedantic, clippy::cargo, clippy::nursery)]
+#![feature(option_result_contains)]
 
 use colored::Colorize;
 use grep::{
@@ -258,6 +259,9 @@ fn validate(paths: [&str; 3]) -> Result<ValidationResults, Box<dyn Error>> {
             };
 
             if !dent.file_type().is_file() {
+                continue
+            }
+            if !dent.path().to_str().contains(&".sol") {
                 continue
             }
 
