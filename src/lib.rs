950f106e03f1a1966b954cf6cb2eeca37f7b7d88
John Feras <jferas@ferasinfotech.com>
2022-11-09 10:27:47 -0500

    fix: don't check non-solidity files (#7)

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use grep::{
     searcher::{sinks::UTF8, BinaryDetection, SearcherBuilder},
 };
 use regex::Regex;
-use std::{error::Error, fmt, fs, process};
+use std::{error::Error, ffi::OsStr, fmt, fs, process};
 use walkdir::WalkDir;
 
 // ========================
@@ -257,7 +257,7 @@ fn validate(paths: [&str; 3]) -> Result<ValidationResults, Box<dyn Error>> {
                 }
             };
 
-            if !dent.file_type().is_file() {
+            if !dent.file_type().is_file() || dent.path().extension() != Some(OsStr::new("sol")) {
                 continue
             }
 
